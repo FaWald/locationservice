eefcb9175395c5fb7648a542b70cd82f9eaced8b
FaWald <63518664+FaWald@users.noreply.github.com>
2023-05-05 09:49:19 +0200

    Update LocationController.java

--- src/main/java/at/htlle/locationservice/LocationController.java
+++ src/main/java/at/htlle/locationservice/LocationController.java
@@ -23,6 +23,13 @@ public class LocationController {
         knownLocations.add(new Location("Mariazell", 47.769722, 15.316667));
     }
 
+    /**
+     * Diese Methode definiert einen Endpunkt für einen GET-Request auf "/locations", der eine Location zurückgibt, deren Name dem angegebenen Namen entspricht.
+     * Wenn keine Location mit diesem Namen gefunden wird, wird ein HTTP-Statuscode "404 Not Found" zurückgegeben.
+     *
+     * @param name der Name der Location, die zurückgegeben werden soll. Wenn dieser Parameter nicht angegeben wird, wird standardmäßig "World" verwendet.
+     * @return eine ResponseEntity-Instanz, die die gefundene Location und den HTTP-Statuscode enthält.
+     */
     @GetMapping("/locations")
     public ResponseEntity<Location> getLocationByName(@RequestParam(value = "name", defaultValue = "World") String name) {
         for (Location location : knownLocations) {
@@ -32,11 +39,20 @@ public class LocationController {
         }
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
+
+
+    /**
+     * Handler-Methode für die "GET /nextknownlocation" Anforderung. Gibt die nächstgelegene bekannte Location
+     * basierend auf der gegebenen Längen- und Breitengradkoordinaten zurück.
+     *
+     * @param latitude  die Längengradkoordinate des aktuellen Standorts
+     * @param longitude die Breitengradkoordinate des aktuellen Standorts
+     * @return ResponseEntity<Location> - eine Antwort mit dem nächstgelegenen bekannten Ort und dem HTTP-Status-Code "OK"
+     */
     @GetMapping("/nextknownlocation")
     public ResponseEntity<Location> getNearestLocation(@RequestParam(value = "latitude") double latitude,
                                                        @RequestParam(value = "longitude") double longitude) {
         Location nearest = Collections.min(knownLocations, Comparator.comparingDouble(l -> l.distanceTo(new Location("", latitude, longitude))));
         return new ResponseEntity<>(nearest, HttpStatus.OK);
     }
-
 }
