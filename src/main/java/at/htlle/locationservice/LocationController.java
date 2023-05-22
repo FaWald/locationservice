03aa849b83c5c2ac41070a2fc126b6b5213107dd
fabian <191wita25@o365.htl-leoben.at>
2023-05-22 11:10:00 +0200

    Beispiel 4 erledigt

--- src/main/java/at/htlle/locationservice/LocationController.java
+++ src/main/java/at/htlle/locationservice/LocationController.java
@@ -20,6 +20,27 @@ public class LocationController {
         knownLocations.add(new Location("Bruck", 47.416667, 15.266667));
         knownLocations.add(new Location("Kapfenberg", 47.433333, 15.316667));
         knownLocations.add(new Location("Mariazell", 47.769722, 15.316667));
+        knownLocations.add(new Location("Graz", 47.066667, 15.45));
+        knownLocations.add(new Location("Vienna", 48.2082, 16.3738));
+        knownLocations.add(new Location("Linz", 48.3064, 14.2858));
+        knownLocations.add(new Location("Graz", 47.0707, 15.4395));
+        knownLocations.add(new Location("Salzburg", 47.8095, 13.0550));
+        knownLocations.add(new Location("Innsbruck", 47.2682, 11.3923));
+        knownLocations.add(new Location("Klagenfurt", 46.6249, 14.3050));
+        knownLocations.add(new Location("Villach", 46.6111, 13.8558));
+        knownLocations.add(new Location("Wels", 48.1575, 14.0289));
+        knownLocations.add(new Location("St. Pölten", 48.2047, 15.6256));
+        knownLocations.add(new Location("Dornbirn", 47.4125, 9.7417));
+        knownLocations.add(new Location("Wiener Neustadt", 47.8151, 16.2465));
+        knownLocations.add(new Location("Bregenz", 47.5031, 9.7471));
+        knownLocations.add(new Location("Eisenstadt", 47.8450, 16.5336));
+        knownLocations.add(new Location("Leonding", 48.2606, 14.2406));
+        knownLocations.add(new Location("Traun", 48.2203, 14.2333));
+        knownLocations.add(new Location("Amstetten", 48.1219, 14.8747));
+        knownLocations.add(new Location("Klosterneuburg", 48.3053, 16.3256));
+        knownLocations.add(new Location("Schwechat", 48.1381, 16.4708));
+        knownLocations.add(new Location("Ternitz", 47.7275, 16.0361));
+        knownLocations.add(new Location("Baden bei Wien", 48.0069, 16.2308));
     }
 
     /**
@@ -57,7 +78,7 @@ public class LocationController {
     @GetMapping("/altitudeof")
     public ResponseEntity<String> altitude(@RequestParam(value = "latitude") double latitude, @RequestParam(value = "longitude") double longitude) {
         // Auslesen von srtm_40_03.asc
-        File file = new File("srtm_40_03.asc");
+        File file = new File("src/main/resources/srtm_40_03.asc");
 
         // Als File - Objekt der SrtmFile.java übergeben für die Datenverarbeitung
         SrtmFile srtmFile = new SrtmFile(file);
@@ -70,7 +91,7 @@ public class LocationController {
         Optional<Double> altitude = srtmFile.getAltitudeForLocation(location);
 
         // JSON-String erstellen
-        String json = "{\"loc\":{\"name\":\"" + name + "\",\"latitude\":\"" + latitude + "\",\"longitude\":\"" + longitude + "\"},\"altitude\":\"" + altitude + "\"}";
+        String json = "{\"loc\":{\"name\":\"" + name + "\",\"latitude\":\"" + latitude + "\",\"longitude\":\"" + longitude + "\"},\"altitude\":\"" + altitude.get() + "\"}";
 
         // ResponseEntity mit JSON-String als Körper zurückgeben
         return ResponseEntity.ok().body(json);
